b768989f053b85551ffafd95de2d181b52672ce1
DanielHabenicht <daniel-habenicht@outlook.de>
2023-01-15 11:29:14 +0000

    make cusum est_length param more flexible

--- src/ldimbenchmark/methods/utils/cusum.py
+++ src/ldimbenchmark/methods/utils/cusum.py
@@ -3,23 +3,31 @@ import pandas as pd
 
 
 def cusum(df, direction="p", delta=4, C_thr=3, est_length="3 days"):
-    """Tabular CUSUM per Montgomery,D. 1996 "Introduction to Statistical Process Control" p318
+    """
+    Tabular CUSUM per Montgomery,D. 1996 "Introduction to Statistical Process Control" p318
        https://www.amazon.com/dp/0470169923
+
+    Parameters
+    ----------
     df        :  data to analyze
     direction :  negative or positive? 'n' or 'p'
     delta     :  parameter to calculate slack value K =>  K = (delta/2)*sigma
     K         :  reference value, allowance, slack value for each pipe
     C_thr     :  threshold for raising flag
-    est_length:  Window for estimating distribution parameters mu and sigma, needs to be longer than one timestep
+    est_length:  Window for estimating distribution parameters mu and sigma, needs to be longer than one timestep (int=hours, str=timedelta, auto=first timestep)
     leak_det  :  Leaks detected
     """
     if est_length == "auto":
         distribution_window = pd.Timedelta(df.index[1] - df.index[0])
+    elif type(est_length) == int or type(est_length) == float:
+        distribution_window = pd.Timedelta(hours=est_length)
     else:
         distribution_window = pd.Timedelta(est_length)
 
     if df.index[1] - df.index[0] > distribution_window:
-        raise ValueError("est_length needs to be longer than one timestep")
+        raise ValueError(
+            f"est_length ({distribution_window}) needs to be longer than one timestep ({df.index[1] - df.index[0]})"
+        )
 
     ar_mean = np.zeros(df.shape[1])
     ar_sigma = np.zeros(df.shape[1])
