fe517ce181c2bbe19ab48c908974becf0f2bf281
DanielHabenicht <daniel-habenicht@outlook.de>
2023-02-24 18:18:06 +0000

    experiments update

--- experiments/auto_hyperparameter.py
+++ experiments/auto_hyperparameter.py
@@ -25,102 +25,136 @@ logging.basicConfig(
 logging.getLogger().setLevel(numeric_level)
 
 
-def run_benchmark(method: LDIMMethodBase, dataset: Dataset, params: Dict):
-    benchmark = LDIMBenchmark(
-        {method.name: {dataset.id: params}},
-        dataset,
-        results_dir="./grid-search",
-        debug=True,
-    )
-    benchmark.add_local_methods(method)
-
-    # execute benchmark
-    benchmark.run_benchmark()
-
-    results = benchmark.evaluate()
-    return results, params
-
-
-class GridSearch:
-    """
-    Search for best hyperparameter fit for a Leakage Detection Method.
-    The cartesian product of all inputs in the param_grid will be tested with the method used.
-    """
-
-    def __init__(self, method: LDIMMethodBase, param_grid: Dict[str, List]) -> None:
-        self.method = method
-        self.param_grid = param_grid
-
-    def detect_offline(self, dataset: Dataset, parallel=True):
-        """
-        Runs the method.
-        """
-        # Create Matrix with all Combinations
-        index = pd.MultiIndex.from_product(
-            self.param_grid.values(), names=self.param_grid.keys()
-        )
-        param_matrix = pd.DataFrame(index=index).reset_index()
-        # print(param_matrix)
-
-        self.results = {}
-        preliminary_results = []
-
-        arguments_list = zip(
-            itertools.repeat(self.method, param_matrix.shape[0]),
-            itertools.repeat(dataset, param_matrix.shape[0]),
-            [param_matrix.iloc[n].to_dict() for n in range(param_matrix.shape[0])],
-        )
-
-        if parallel:
-            with Pool(processes=cpu_count() - 1) as p:
-                jobs = [
-                    p.apply_async(func=run_benchmark, args=arguments)
-                    for arguments in arguments_list
-                ]
-                for job in tqdm(jobs):
-                    job_result, params = job.get()  # Execute Job
-                    job_result["params"] = [params]
-                    print(job_result)
-                    preliminary_results.append(job_result)
-        else:
-            for arguments in tqdm(arguments_list):
-                job_result, params = run_benchmark(*arguments)
-                job_result["params"] = params
-                preliminary_results.append(job_result)
-
-        results = pd.concat(preliminary_results)
-        results = results.sort_values(by=["F1"])
-        results.to_csv("results.csv")
-
-    def detect_online(self, dataset: Dataset):
-        pass
+param_grid = {
+    "lila": {
+        "est_length": [1, 72],
+        "C_threshold": [3.0],
+        "delta": [4.0],
+        #     "est_length": np.arange(1, 100, 8).tolist(),
+        #     "C_threshold": np.arange(-0.3, 1, 0.05).tolist(),
+        #     "delta": np.arange(0, 10, 0.5).tolist(),
+    },
+    "mnf": {
+        "gamma": np.arange(-0.3, 1, 0.05).tolist(),
+        "window": [1, 5, 10, 20],
+    },
+}
 
 
-datasets = DatasetLibrary("tests/test_data/datasets").download(DATASETS.BATTLEDIM)
+datasets = DatasetLibrary("test_data/datasets").download(DATASETS.BATTLEDIM)
 
+
+benchmark = LDIMBenchmark(
+    hyperparameters=param_grid,
+    datasets=datasets,
+    results_dir="./grid-search",
+    debug=True,
+    multi_parameters=True,
+)
+benchmark.add_docker_methods(["ghcr.io/ldimbenchmark/mnf:0.1.20"])
+
+# execute benchmark
+benchmark.run_benchmark(parallel=True, parallel_max_workers=3)
+
+results = benchmark.evaluate()
+
+
+# def run_benchmark(method: LDIMMethodBase, dataset: Dataset, params: Dict):
+#     benchmark = LDIMBenchmark(
+#         {method.name: {dataset.id: params}},
+#         dataset,
+#         results_dir="./grid-search",
+#         debug=True,
+#     )
+#     benchmark.add_local_methods(method)
+
+#     # execute benchmark
+#     benchmark.run_benchmark()
+
+#     results = benchmark.evaluate()
+#     return results, params
+
+
+# class GridSearch:
+#     """
+#     Search for best hyperparameter fit for a Leakage Detection Method.
+#     The cartesian product of all inputs in the param_grid will be tested with the method used.
+#     """
+
+#     def __init__(self, method: LDIMMethodBase, param_grid: Dict[str, List]) -> None:
+#         self.method = method
+#         self.param_grid = param_grid
+
+#     def detect_offline(self, dataset: Dataset, parallel=True):
+#         """
+#         Runs the method.
+#         """
+#         # Create Matrix with all Combinations
+#         index = pd.MultiIndex.from_product(
+#             self.param_grid.values(), names=self.param_grid.keys()
+#         )
+#         param_matrix = pd.DataFrame(index=index).reset_index()
+#         # print(param_matrix)
+
+#         self.results = {}
+#         preliminary_results = []
+
+#         arguments_list = zip(
+#             itertools.repeat(self.method, param_matrix.shape[0]),
+#             itertools.repeat(dataset, param_matrix.shape[0]),
+#             [param_matrix.iloc[n].to_dict() for n in range(param_matrix.shape[0])],
+#         )
+
+#         if parallel:
+#             with Pool(processes=cpu_count() - 1) as p:
+#                 jobs = [
+#                     p.apply_async(func=run_benchmark, args=arguments)
+#                     for arguments in arguments_list
+#                 ]
+#                 for job in tqdm(jobs):
+#                     job_result, params = job.get()  # Execute Job
+#                     job_result["params"] = [params]
+#                     print(job_result)
+#                     preliminary_results.append(job_result)
+#         else:
+#             for arguments in tqdm(arguments_list):
+#                 job_result, params = run_benchmark(*arguments)
+#                 job_result["params"] = params
+#                 preliminary_results.append(job_result)
+
+#         results = pd.concat(preliminary_results)
+#         results = results.sort_values(by=["F1"])
+#         results.to_csv("results.csv")
+
+#     def detect_online(self, dataset: Dataset):
+#         pass
+
+
+# datasets = DatasetLibrary("tests/test_data/datasets").download(DATASETS.BATTLEDIM)
+
+
+# # param_grid = {
+# #     "gamma": np.arange(-0.3, 1, 0.05).tolist(),
+# #     "window": [1, 5, 10, 20],
+# # }
+# # grid_search = GridSearch(MNF(), param_grid)
 
 # param_grid = {
-#     "gamma": np.arange(-0.3, 1, 0.05).tolist(),
-#     "window": [1, 5, 10, 20],
+#     "est_length": np.arange(1, 100, 8).tolist(),
+#     "C_threshold": np.arange(-0.3, 1, 0.05).tolist(),
+#     "delta": np.arange(0, 10, 0.5).tolist(),
 # }
-# grid_search = GridSearch(MNF(), param_grid)
-
-param_grid = {
-    "est_length": np.arange(1, 100, 8).tolist(),
-    "C_threshold": np.arange(-0.3, 1, 0.05).tolist(),
-    "delta": np.arange(0, 10, 0.5).tolist(),
-}
-param_grid = {
-    "est_length": [1, 72],
-    "C_threshold": [3.0],
-    "delta": [4.0],
-}
-grid_search = GridSearch(LILA(), param_grid)
+# param_grid = {
+#     "est_length": [1, 72],
+#     "C_threshold": [3.0],
+#     "delta": [4.0],
+# }
+# grid_search = GridSearch(LILA(), param_grid)
 
 
-grid_search.detect_offline(datasets[0].loadData().loadBenchmarkData())
+# grid_search.detect_offline(datasets[0].loadData().loadBenchmarkData())
 
-print(grid_search.results)
+# print(grid_search.results)
 
 # from sklearn import svm, datasets
 # from sklearn.model_selection import GridSearchCV
--- experiments/sensitivity_analyisis.py
+++ experiments/sensitivity_analyisis.py
@@ -2,99 +2,93 @@
 # %load_ext autoreload
 # %autoreload 2
 # Fix https://github.com/numpy/numpy/issues/5752
-if __name__ == "__main__":
-    import os
-    from ldimbenchmark.datasets.analysis import DatasetAnalyzer
-    from ldimbenchmark.datasets import Dataset, DatasetLibrary, DATASETS
-    from ldimbenchmark.datasets.derivation import DatasetDerivator
-    from ldimbenchmark.generator import generateDatasetForTimeSpanDays
-    from ldimbenchmark.methods import MNF, LILA
 
-    from ldimbenchmark.benchmark import LDIMBenchmark
-    import logging
-    from matplotlib import pyplot as plt
-    import numpy as np
+import os
+from ldimbenchmark.datasets.analysis import DatasetAnalyzer
+from ldimbenchmark.datasets import Dataset, DatasetLibrary, DATASETS
+from ldimbenchmark.datasets.derivation import DatasetDerivator
+from ldimbenchmark.generator import generateDatasetForTimeSpanDays
+from ldimbenchmark.methods import MNF, LILA
 
-    test_data_folder = "test_data"
-    test_data_folder_datasets = os.path.join("test_data", "datasets")
+from ldimbenchmark.benchmark import LDIMBenchmark
+import logging
+from matplotlib import pyplot as plt
+import numpy as np
 
-    # %%
+test_data_folder = "test_data"
+test_data_folder_datasets = os.path.join("test_data", "datasets")
 
-    # Download
-    battledim_dataset = DatasetLibrary(test_data_folder_datasets).download(
-        DATASETS.BATTLEDIM
-    )[0]
+# %%
 
-    # %%
+# Download
+battledim_dataset = DatasetLibrary(test_data_folder_datasets).download(
+    DATASETS.BATTLEDIM
+)[0]
 
-    generated_dataset_path = os.path.join(test_data_folder_datasets, "synthetic-days-9")
-    generateDatasetForTimeSpanDays(90, generated_dataset_path)
-    generated_dataset = Dataset(generated_dataset_path)
+# %%
 
-    # %%
+generated_dataset_path = os.path.join(test_data_folder_datasets, "synthetic-days-9")
+generateDatasetForTimeSpanDays(90, generated_dataset_path)
+generated_dataset = Dataset(generated_dataset_path)
 
-    dataset = battledim_dataset
+# %%
 
-    allDerivedDatasets = []
-    allDerivedDatasets.append(dataset)
+dataset = battledim_dataset
 
-    # %%
+allDerivedDatasets = []
+allDerivedDatasets.append(dataset)
 
-    derivator = DatasetDerivator([dataset], os.path.join(test_data_folder_datasets))
-    derivedDatasets = derivator.derive_data(
-        "pressures", "precision", [0.01, 0.1, 0.5, 1.0]
-    )
-    allDerivedDatasets = allDerivedDatasets + derivedDatasets
+# %%
 
-    # #%%
+derivator = DatasetDerivator([dataset], os.path.join(test_data_folder_datasets))
+derivedDatasets = derivator.derive_data("pressures", "precision", [0.01, 0.1, 0.5, 1.0])
+allDerivedDatasets = allDerivedDatasets + derivedDatasets
 
-    # derivator = DatasetDerivator([dataset], os.path.join(test_data_folder_datasets))
-    # derivedDatasets = derivator.derive_data("pressures", "downsample", [60*10, 60*20, 60*30])
-    # allDerivedDatasets = allDerivedDatasets + derivedDatasets
+# #%%
 
-    # #%%
+# derivator = DatasetDerivator([dataset], os.path.join(test_data_folder_datasets))
+# derivedDatasets = derivator.derive_data("pressures", "downsample", [60*10, 60*20, 60*30])
+# allDerivedDatasets = allDerivedDatasets + derivedDatasets
 
-    # derivator = DatasetDerivator([dataset], os.path.join(test_data_folder_datasets))
-    # derivedDatasets = derivator.derive_data("pressures", "sensitivity", [1, 0.5, 0.1])
-    # allDerivedDatasets = allDerivedDatasets + derivedDatasets
+# #%%
 
-    # %%
+# derivator = DatasetDerivator([dataset], os.path.join(test_data_folder_datasets))
+# derivedDatasets = derivator.derive_data("pressures", "sensitivity", [1, 0.5, 0.1])
+# allDerivedDatasets = allDerivedDatasets + derivedDatasets
 
-    logLevel = "INFO"
+# %%
 
-    numeric_level = getattr(logging, logLevel, None)
-    if not isinstance(numeric_level, int):
-        raise ValueError("Invalid log level: %s" % logLevel)
+logLevel = "INFO"
 
-    logging.basicConfig(level=numeric_level, handlers=[logging.StreamHandler()])
-    logging.getLogger().setLevel(numeric_level)
+numeric_level = getattr(logging, logLevel, None)
+if not isinstance(numeric_level, int):
+    raise ValueError("Invalid log level: %s" % logLevel)
 
-    local_methods = [LILA()]
+logging.basicConfig(level=numeric_level, handlers=[logging.StreamHandler()])
+logging.getLogger().setLevel(numeric_level)
 
-    hyperparameters = {
-        "lila": {
-            "est_length": 24,
-            "C_threshold": 10000.0,
-            "delta": 2.0,
-        }
+hyperparameters = {
+    "lila": {
+        "est_length": 24,
+        "C_threshold": 10000.0,
+        "delta": 2.0,
     }
+}
 
-    benchmark = LDIMBenchmark(
-        hyperparameters,
-        allDerivedDatasets,
-        # derivedDatasets[0],
-        # dataset,
-        results_dir="./benchmark-results",
-        debug=True,
-    )
-    benchmark.add_local_methods(local_methods)
+benchmark = LDIMBenchmark(
+    hyperparameters,
+    allDerivedDatasets,
+    # derivedDatasets[0],
+    # dataset,
+    results_dir="./benchmark-results",
+    debug=True,
+)
+benchmark.add_docker_methods(["ghcr.io/ldimbenchmark/lila:0.1.20"])
 
-    benchmark.run_benchmark(parallel=True, parallel_max_workers=4)
+benchmark.run_benchmark(parallel=True, parallel_max_workers=4)
 
-    benchmark.evaluate(True)
+benchmark.evaluate(True)
 
-    # %%
+# %%
 
-    benchmark.evaluate(True)
-
-    # benchmark.evaluate(True, write_results=True, generate_plots=True)
+# benchmark.evaluate(True, write_results=True, generate_plots=True)
