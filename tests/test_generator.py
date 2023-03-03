5ee1b5b2ea836d55c879740d6b3a3be69cf86274
DanielHabenicht <daniel-habenicht@outlook.de>
2023-03-03 12:40:50 +0000

    speed up tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -12,24 +12,26 @@ import os
 
 
 def test_generator_time():
-    out_dir = os.path.join(TEST_DATA_FOLDER_DATASETS_GENERATED, "synthetic-days-90")
-    generateDatasetForTimeSpanDays(90, out_dir)
+    out_dir = os.path.join(TEST_DATA_FOLDER_DATASETS_GENERATED, "synthetic-days-10")
+    generateDatasetForTimeSpanDays(4, out_dir)
     dataset = Dataset(out_dir).loadData()
 
 
 def test_generator_junction():
-    out_dir = os.path.join(TEST_DATA_FOLDER_DATASETS_GENERATED, "synthetic-j-30")
-    generateDatasetForJunctionNumber(30, out_dir)
+    out_dir = os.path.join(TEST_DATA_FOLDER_DATASETS_GENERATED, "synthetic-j-4")
+    generateDatasetForJunctionNumber(4, out_dir)
     dataset = Dataset(out_dir).loadData()
 
 
 def test_generator_set_time():
-    generateDatasetsForTimespan(
-        1, 10, os.path.join(TEST_DATA_FOLDER_DATASETS_GENERATED)
-    )
+    one_out_dir = os.path.join(TEST_DATA_FOLDER_DATASETS_GENERATED, "synthetic-days-2")
+    generateDatasetsForTimespan(1, 3, os.path.join(TEST_DATA_FOLDER_DATASETS_GENERATED))
+    dataset = Dataset(one_out_dir).loadData()
 
 
 def test_generator_set_junctions():
+    one_out_dir = os.path.join(TEST_DATA_FOLDER_DATASETS_GENERATED, "synthetic-j-6")
     generateDatasetsForJunctions(
-        4, 10, os.path.join(TEST_DATA_FOLDER_DATASETS_GENERATED)
+        5, 7, os.path.join(TEST_DATA_FOLDER_DATASETS_GENERATED)
     )
+    dataset = Dataset(one_out_dir).loadData()
