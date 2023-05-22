2a6c15145fa77a1b9830377d46baab40272c722e
DanielHabenicht <daniel-habenicht@outlook.de>
2023-05-22 21:34:31 +0000

    fix: exponential operator

--- src/ldimbenchmark/methods/test_complexity_exponential.py
+++ src/ldimbenchmark/methods/test_complexity_exponential.py
@@ -29,15 +29,15 @@ class TestExponentialComplexityLeakageDetectionMethod(LDIMMethodBase):
         )
 
     def prepare(self, train_data: BenchmarkData) -> None:
-        sleep(2 ^ len(train_data.demands[list(train_data.demands.keys())[0]]) / 10)
+        sleep(2 ** len(train_data.demands[list(train_data.demands.keys())[0]]) / 10)
         return
 
     def detect_offline(
         self, evaluation_data: BenchmarkData
     ) -> List[BenchmarkLeakageResult]:
         sleep(
-            2
-            ^ len(evaluation_data.demands[list(evaluation_data.demands.keys())[0]]) / 10
+            2 ** len(evaluation_data.demands[list(evaluation_data.demands.keys())[0]])
+            / 10
         )
         return []
 
