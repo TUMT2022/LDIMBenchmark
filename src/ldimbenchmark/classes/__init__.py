613ec00e603a44f939ace79b8b47157d7210eddd
DanielHabenicht <daniel-habenicht@outlook.de>
2023-07-16 14:41:40 +0000

    fix: import error

--- src/ldimbenchmark/classes/__init__.py
+++ src/ldimbenchmark/classes/__init__.py
@@ -4,8 +4,6 @@ Main Module
 
 from importlib import metadata
 
-__version__ = metadata.version(__package__ or __name__)
-
 from .BenchmarkData import *
 from .BenchmarkLeakageResult import *
 from .LDIMMethodBase import *
